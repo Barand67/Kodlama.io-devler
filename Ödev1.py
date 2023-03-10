b4579695d80cb491f4b15465f8c369d6554a977e
Barand67 <96434748+Barand67@users.noreply.github.com>
2023-03-10 19:40:52 +0300

    Ödev1

---
+++ Ödev1.py
@@ -0,0 +1,16 @@
+#1)
+#Veri Tipkeri
+#int -float- str - bool - list
+
+a=5 # İnt
+a=5.5 #float
+a='elma' # str
+a=True #bool
+a=["elma","armut","muz"] # list
+
+
+#2)
+Tamamlama yüzdesine int diyebiliriz
+1.Gün ders kımına list diyebiliriz
+Tamalama veya tamamlamam drumuna öre True veya False diyebiliriz
+ 
